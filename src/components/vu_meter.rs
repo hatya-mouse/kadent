c1ff701ec04571dd3cf05e04a229763d86409a27
Hatya-mouse <shuntar30@gmail.com>
2026-05-01 11:14:22 +0900

    Center and add the background for the VU Meter

--- src/components/vu_meter.rs
+++ src/components/vu_meter.rs
@@ -1,18 +1,63 @@
+use crate::theme;
 use eframe::egui;
 
-pub(crate) fn vu_meter(ui: &egui::Ui, value: f32, width: f32, height: f32, corner_radius: f32) {
-    let painter = ui.painter();
+const METER_GAP: f32 = 2.0;
 
-    let bar_rect = egui::Rect::from_min_size(
-        ui.cursor().min,
-        egui::vec2(width * value.clamp(0.0, 1.0), height),
+pub(crate) fn vu_meter(ui: &mut egui::Ui, channels: &[f32], size: egui::Vec2, corner_radius: u8) {
+    let (rect, _) = ui.allocate_exact_size(size, egui::Sense::empty());
+    let row_height = size.y / channels.len() as f32;
+    let height_with_gap = row_height - METER_GAP;
+
+    // Draw the background
+    ui.painter().rect_filled(
+        rect,
+        corner_radius,
+        theme::button_bg(ui.visuals().dark_mode),
     );
-    let fill_color = if value < 0.6 {
-        egui::Color32::GREEN
-    } else if value < 0.85 {
-        egui::Color32::YELLOW
-    } else {
-        egui::Color32::RED
-    };
-    painter.rect_filled(bar_rect, corner_radius, fill_color);
+
+    for (row, value) in channels.iter().enumerate() {
+        let row_rect = egui::Rect::from_min_size(
+            rect.min + egui::vec2(0.0, row_height * row as f32 + METER_GAP / 2.0),
+            // Leave space for the corner radius on the right side
+            egui::vec2(
+                (size.x - corner_radius as f32) * value.clamp(0.0, 1.0),
+                height_with_gap,
+            ),
+        );
+        let fill_color = if *value < 0.6 {
+            egui::Color32::GREEN
+        } else if *value < 0.85 {
+            egui::Color32::YELLOW
+        } else {
+            egui::Color32::RED
+        };
+
+        // Apply corner radius only to the first and last rows
+        // If there's only one channel, apply the radius to all corners
+        let row_radius = if channels.len() == 1 {
+            egui::CornerRadius {
+                nw: corner_radius,
+                ne: 0,
+                sw: corner_radius,
+                se: 0,
+            }
+        } else if row == 0 {
+            egui::CornerRadius {
+                nw: corner_radius,
+                ne: 0,
+                sw: 0,
+                se: 0,
+            }
+        } else if row == channels.len() - 1 {
+            egui::CornerRadius {
+                nw: 0,
+                ne: 0,
+                sw: corner_radius,
+                se: 0,
+            }
+        } else {
+            egui::CornerRadius::ZERO
+        };
+        ui.painter().rect_filled(row_rect, row_radius, fill_color);
+    }
 }
--- src/ui/editor/toolbar/vu_meter.rs
+++ src/ui/editor/toolbar/vu_meter.rs
@@ -3,14 +3,15 @@ use eframe::egui;
 use ringbuf::traits::Consumer;
 
 impl EditorUi {
-    pub(super) fn vu_meter(&mut self, ui: &egui::Ui) {
+    pub(super) fn vu_meter(&mut self, ui: &mut egui::Ui) {
         let channels = self.project.audio_ctx.channels;
         self.ui_state.last_vu_value.resize(channels, 0.0);
         for channel in 0..channels {
             if let Some(v) = self.thread_handle.vu_consumer.try_pop() {
                 self.ui_state.last_vu_value[channel] = v;
             };
-            vu_meter(ui, self.ui_state.last_vu_value[channel], 200.0, 28.0, 4.0);
         }
+
+        vu_meter(ui, &self.ui_state.last_vu_value, egui::vec2(200.0, 28.0), 4);
     }
 }
