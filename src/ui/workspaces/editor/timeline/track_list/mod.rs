07a1b21c14f89c97dc8c8cefd0f10efb585edf3a
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-06-27 14:57:00 +0900

    feat: improve add track button design

--- src/ui/workspaces/editor/timeline/track_list/mod.rs
+++ src/ui/workspaces/editor/timeline/track_list/mod.rs
@@ -3,7 +3,6 @@ mod track_dialog;
 use crate::{
     core::metadata::TrackType,
     ui::{
-        components::icon_button::toolbar_icon_button,
         theme,
         workspaces::{
             EditorUi,
@@ -11,13 +10,15 @@ use crate::{
         },
     },
 };
-use eframe::egui;
+use eframe::egui::{self, include_image};
 
 impl EditorUi {
     pub(super) fn track_list_panel(&mut self, ui: &mut egui::Ui) {
         egui::Frame::new()
             .fill(theme::primary_bg(ui.visuals().dark_mode))
             .show(ui, |ui| {
+                let list_width = ui.available_width();
+
                 for track_id in &self.proj_ctx.project_meta.track_order {
                     if let Some(track_meta) = self.proj_ctx.project_meta.tracks.get(track_id) {
                         let bg_color = if Some(track_id) == self.ui_state.selected_track.as_ref() {
@@ -28,7 +29,7 @@ impl EditorUi {
 
                         let track_frame = egui::Frame::new().fill(bg_color).show(ui, |ui| {
                             ui.horizontal(|ui| {
-                                ui.set_min_width(ui.available_width());
+                                ui.set_min_width(list_width);
 
                                 // Draw track color
                                 let (rect, _) = ui.allocate_exact_size(
@@ -49,20 +50,46 @@ impl EditorUi {
                         // Select the track on click
                         let response =
                             ui.allocate_rect(track_frame.response.rect, egui::Sense::click());
+                        ui.painter().line_segment(
+                            [response.rect.left_bottom(), response.rect.right_bottom()],
+                            theme::border(ui.visuals().dark_mode),
+                        );
                         if response.clicked() {
                             self.ui_state.set_selected_track(*track_id);
                         }
                     }
                 }
 
-                if toolbar_icon_button(
-                    ui,
-                    egui::Image::new(egui::include_image!(
-                        "../../../../../../assets/icons/plus.svg"
-                    )),
-                )
-                .clicked()
-                {
+                let track_frame = egui::Frame::new()
+                    .fill(theme::secondary_bg(ui.visuals().dark_mode))
+                    .inner_margin(egui::Margin::symmetric(8, 2))
+                    .show(ui, |ui| {
+                        let desired_size = egui::vec2(list_width, 24.0);
+                        let layout = egui::Layout::left_to_right(egui::Align::Center);
+                        ui.allocate_ui_with_layout(desired_size, layout, |ui| {
+                            ui.set_min_width(list_width);
+
+                            ui.add(
+                                egui::Image::new(include_image!(
+                                    "../../../../../../assets/icons/plus.svg"
+                                ))
+                                .fit_to_exact_size(egui::vec2(20.0, 20.0))
+                                .tint(theme::primary_fg(ui.visuals().dark_mode)),
+                            );
+                            ui.label(
+                                egui::RichText::new("Add Track")
+                                    .size(theme::normal_font_size())
+                                    .color(theme::primary_fg(ui.visuals().dark_mode)),
+                            );
+                        });
+                    });
+
+                let response = ui.allocate_rect(track_frame.response.rect, egui::Sense::click());
+                ui.painter().line_segment(
+                    [response.rect.left_bottom(), response.rect.right_bottom()],
+                    theme::border(ui.visuals().dark_mode),
+                );
+                if response.clicked() {
                     self.ui_state.dialog_state = DialogState::AddTrack(AddTrackState {
                         selected_track_type: TrackType::Audio,
                         name: String::new(),
