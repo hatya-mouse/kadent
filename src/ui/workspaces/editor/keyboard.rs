3ee11fdb9fa3fa11092cbad0de58ca27489b8ccd
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-06-28 14:42:05 +0900

    feat: use i.modifiers.command instead of custom OS conditional compilation

--- src/ui/workspaces/editor/keyboard.rs
+++ src/ui/workspaces/editor/keyboard.rs
@@ -5,6 +5,8 @@ use crate::{
     ui::workspaces::{EditorUi, editor::state::Selection},
 };
 use eframe::egui;
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+use eframe::egui::InputState;
 
 impl EditorUi {
     pub(super) fn handle_keyboard(&mut self, ui: &egui::Ui) {
@@ -31,22 +33,10 @@ impl EditorUi {
             }
         }
 
-        let save = ui.input(|i| is_command_pressed(ui) && i.key_pressed(egui::Key::S));
+        let save = ui.input(|i| i.modifiers.command && i.key_pressed(egui::Key::S));
         if save {
             // Save the projects and opened KASL programs
             self.push_action(EditorAction::SaveAll);
         }
     }
-}
-
-/// Returns true if the command key is pressed on macOS/iOS, or the control key is pressed on other platforms.
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-fn is_command_pressed(ui: &egui::Ui) -> bool {
-    ui.input(|i| i.modifiers.command)
-}
-
-/// Returns true if the command key is pressed on macOS/iOS, or the control key is pressed on other platforms.
-#[cfg(not(any(target_os = "macos", target_os = "ios")))]
-fn is_command_pressed(ui: &egui::Ui) -> bool {
-    ui.input(|i| i.modifiers.ctrl)
 }
